a3f9d22c562614e2926fdf69631d4ca760ebb010
Yossaphol <69375434+Yossaphol@users.noreply.github.com>
2021-09-20 19:54:23 +0700

    Delete coding directory

--- coding/amongus/color/script.js
+++
@@ -1,8 +0,0 @@
-
-let root = document.querySelector(':root');
-
-// color changing function
-function changeColor(color, shade){
-    root.style.setProperty('--color', color)
-    root.style.setProperty('--shade', shade)
-}
